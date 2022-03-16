c226321c14429b8ea839382308a16ce8dc5f44f2
stevecyj <ycsteven.tsao@gmail.com>
2022-03-16 16:26:50 +0800

    [tooblar]:for image

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <QuillEditor
+    theme="snow"
+    toolbar="full"
+    :modules="modules"
+    @editorChange="imageHandler($event)"
+  />
+  <br />
+  <div class="btn btn-primary">Post Preview</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { QuillEditor } from '@vueup/vue-quill';
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import BlotFormatter from 'quill-blot-formatter';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'App',
+    components: {
+        QuillEditor,
+    },
+    data() {
+        return {
+            modules: {
+                toolbar: {
+                    container: [['image']],
+                    handlers: {
+                        image: this.imageHandler,
+                    },
+                },
+                name: 'blotFormatter',
+                module: BlotFormatter,
+            },
+        };
+    },
+    methods: {
+        /**
+         * The method for dealing with the image put in the editor
+         * to upload the file to firebase storage
+         */
+        imageHandler(event) {
+            let rangy = event.range;
+            console.log(rangy);
+            console.log(rangy.Range);
+        },
+    },
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
